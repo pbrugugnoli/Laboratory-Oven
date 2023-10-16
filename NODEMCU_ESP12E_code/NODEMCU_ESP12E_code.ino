dae5e25dd25fd6c200b0c7f77fe881b12261e3e2
Paulo Eduardo Brugugnoli <pbrugugnoli@hotmail.com>
2023-10-16 20:10:33 -0300

    Version 3- Adafruit.io publish and subscribe

--- NODEMCU_ESP12E_code/NODEMCU_ESP12E_code.ino
+++ NODEMCU_ESP12E_code/NODEMCU_ESP12E_code.ino
@@ -1,31 +1,17 @@
-// Laboratory Oven control system
-// Base on several libraries
-// - read/write from/to google sheets: https://github.com/StorageB/Google-Sheets-Logging
-// - InfluxDB
+// Laboratory Oven control system - Version 3
+// - Adafruit.io service 
 // - PID control
 
 #include <Arduino.h> 
 #include "C:\Users\pbrug\Documents\01. PEB Personal\Credenciais Cloud\SecurityGit\credentials.h"
-//#include <ESP8266WiFi.h>
-#if defined(ESP32)
-  #include <WiFiMulti.h>
-  WiFiMulti wifiMulti;
-  #define DEVICE "ESP32"
-#elif defined(ESP8266)
-  #include <ESP8266WiFiMulti.h>
-  ESP8266WiFiMulti wifiMulti;
-  #define DEVICE "NodeMCU-ESP12E"
-#endif
+#include <ESP8266WiFi.h>
 
 #include <EEPROM.h>
 #include <ArduinoOTA.h>
 #include <ESP8266WebServer.h>
 
-#include "HTTPSRedirect.h"
-#include <InfluxDbClient.h>
-#include <InfluxDbCloud.h>
-#include "DHTesp.h"
 #include <ArduinoJson.h>
+#include "DHTesp.h"
 #include <PID_v1.h>
 
 // DISABLE DEBUG FOR PRODUCTION
@@ -53,28 +39,20 @@
 #endif
 
 // Configuring INFLUXDB
-// Time zone info
-#define TZ_INFO "UTC-3"
 
-// Influx parameters
-//#define INFLUXDB_TOKEN ... in credentials file
-#define INFLUXDB_URL "http://192.168.27.127:8086"
-#define INFLUXDB_ORG "PEB"
-#define INFLUXDB_BUCKET "LabOven"
+// Declare feeds to publish sensor data 
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, wifi_ssid, wifi_password);
+AdafruitIO_Feed *feed_tempA = io.feed("laboven.tempa");
+AdafruitIO_Feed *feed_tempB = io.feed("laboven.tempb");
+AdafruitIO_Feed *feed_powerA = io.feed("laboven.powera");
+AdafruitIO_Feed *feed_powerB = io.feed("laboven.powerb");
+AdafruitIO_Feed *feed_targetA = io.feed("laboven.targeta");
+AdafruitIO_Feed *feed_targetB = io.feed("laboven.targetb");
 
-// Declare InfluxDB client instance with preconfigured InfluxCloud certificate
-InfluxDBClient influx_client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
-
-// Declare Data points = SensorData as a table with historical IOT data, and SensorConfig as configuration table 
-Point dpLabA("SensorData");
-Point dpLabB("SensorData");
-Point dpMCU("SensorData");
-//Point datapoint("LabOven");
-
-// Declare influx query variables
-String influx_query;
-String field, sensor, device;
-double value;
+AdafruitIO_Feed *feed_delay = io.feed("laboven.delay");
+AdafruitIO_Feed *feed_configA = io.feed("laboven.configa");
+AdafruitIO_Feed *feed_configB = io.feed("laboven.configb");
 
 
 // Configuration for fallback access point 
@@ -114,16 +92,8 @@ int ENB = D8;
 int IN3 = D3;
 int IN4 = D4;
 
-// Enter command (insert_row or append_row) and your Google Sheets sheet name (default is Sheet1):
-char payload_base[] = "{\"command\": \"insert_row\", \"sheet_name\": \"DataLog\", \"values\": \"%10u,%5.1f,%5.1f,%5.1f,%5.1f,%5.1f,%5.1f,%2s,%10u\"}";
-char payload[124] = "";
-
-// Google Sheets setup (do not edit)
-const char* host = "script.google.com";
-const int httpsPort = 443;
-const char* fingerprint = "";
-String url = String("/macros/s/") + GScriptId + "/exec";
-HTTPSRedirect* client = nullptr;
+// variables do retrieve and parse data from Adafruit messages
+// for some unknown (yet) reason, the receive string has maximum length=52
 String json_response;
 DynamicJsonDocument doc(1024);
 
@@ -181,8 +151,8 @@ PID myPIDB (&temperatureB, &powerB, &targetTempB, Kp_B, Ki_B, Kd_B, DIRECT);
 // ------------------------------------------
 void setup() {
   // Serial Communication
-  Serial.begin(9600);        
-  delay(10000);
+  Serial.begin(115200);        
+   while(! Serial);
    
   // init EEPROM object 
   // to read/write wifi configuration.
@@ -210,22 +180,30 @@ void setup() {
   // Onboard LED
   pinMode(LED_BUILTIN, OUTPUT);  // GPIO 16
 
-  // Get time from external service
-  debugV("Synchronize time...");
-  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");  
 
-  // Connect to Influxdb
-  debugV("Connecting to influxDB...");
-  if (influx_client.validateConnection()) {
-    debugV("Connected to InfluxDB: %s\n", influx_client.getServerUrl());
-  } else {
-    debugE("InfluxDB connection failed: %s\n", influx_client.getLastErrorMessage() );
+  // Connect to Adafruit.io
+  debugI("Connecting to Adafruit IO");
+
+  io.connect();
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    debugV("Trying ...");
+    delay(500);
   }
 
-  // Add tags to the data point
-  dpLabA.addTag("sensor", "BoxA");
-  dpLabB.addTag("sensor", "BoxB");
-  dpMCU.addTag("device", DEVICE);
+  // we are connected
+  debugI("Connected to Adadruit.io: %s", io.statusText());
+
+  // subscribe
+  feed_delay->onMessage(handleMessage_delay);
+  feed_configA->onMessage(handleMessage_configA);
+  feed_configB->onMessage(handleMessage_configB);
+
+  // get last published values
+  feed_delay->get();
+  feed_configA->get();
+  feed_configB->get();
 
   // Set Sensor A and B
   debugV("Setting up sensors...");
@@ -246,16 +224,6 @@ void setup() {
   pinMode(IN4, OUTPUT); 
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-
-  // GET data from google sheet (through out influxdb) - initialize target variables
-  // read and write data into influxdb
-  if (WiFi.status() != WL_CONNECTED){
-    debugI("Wifi was disconnected... Trying to reconnect");
-    connectToWiFi();
-  }
-  if (WiFi.status() == WL_CONNECTED){
-    get_data_from_influxdb();
-  }
 
   previousTime = millis(); 
 
@@ -284,6 +252,9 @@ void loop() {
   // Give processing time for the webserver.
   server.handleClient();
 
+  // io.run(); is required for all sketches to keep the client connected to io.adafruit.com, and processes any incoming data.
+  io.run();  
+
   // After each time_delay log retrieve parameters from google sheet and write log
   currentTime = millis();                           // get current time
   elapsedTime = (currentTime - previousTime)/1000;  // in seconds
@@ -297,7 +268,7 @@ void loop() {
       connectToWiFi();
     }
     if (WiFi.status() == WL_CONNECTED){
-      post_data_into_influxdb();
+      publish_data_to_adafruit_io();
     }
 
     previousTime = currentTime;      // the last "trigger time" is stored 
@@ -317,77 +288,6 @@ void loop() {
 // -------------------------------------------------------------------
 // MAIN LABORATORY OVEN FUNCTIONS (to be replace for a class+methods)
 // -------------------------------------------------------------------
-void get_data_from_sheets(HTTPSRedirect * client){
-
-  debugI("Getting data from Google Sheet...");
-
-  if(client->GET(url, host, false)){ 
-    json_response = client->getResponseBody(); // json_response was declared as global to avoid mem fragmentation
-
- // Decode JSON/Extract values
-    //DynamicJsonDocument doc(1024);          // doc as a global var to avoid mem fragmentation
-    debugI(" - Starting deserialization...");
-    deserializeJson(doc, json_response);  
-
-    debugI(" - Starting retrieving...");
-    targetTempA = doc["targetA"];
-    targetTempB = doc["targetB"];
-    time_delay = doc["delay"];
-    max_duty_cycleA = (int) doc["max_duty_cycleA"];
-    max_duty_cycleB = (int) doc["max_duty_cycleB"];
-
-    if ( Kp_A != doc["Kp_A"] ||  Kd_A != doc["Kd_A"] || Ki_A != doc["Ki_A"]){
-      flagChangeKA = true;
-      Kp_A = doc["Kp_A"];
-      Kd_A = doc["Kd_A"];
-      Ki_A = doc["Ki_A"];
-    } else {
-      flagChangeKA = false;
-    }
-
-    if ( Kp_B != doc["Kp_B"] ||  Kd_B != doc["Kd_B"] || Ki_B != doc["Ki_B"]){
-      flagChangeKB = true;
-      Kp_B = doc["Kp_B"];
-      Kd_B = doc["Kd_B"];
-      Ki_B = doc["Ki_B"];
-    } else {
-      flagChangeKB = false;
-    }
-
-    debugV(" - Data >> Target A:%f\tTarget B:%f\tDelay:%d\tDuty A:%d\tDuty B:%d", targetTempA, targetTempB, time_delay, max_duty_cycleA, max_duty_cycleB);
-    debugV(" - Data >> A(Kp, Ki, Kd):(%f , %f, %f)\tB(Kp, Ki, Kd):(%f , %f, %f)\n", Kp_A, Ki_A, Kd_A, Kp_B, Ki_B, Kd_B);
-
-  }
-}
-
-void post_data_to_sheets(HTTPSRedirect * client){
-
-  debugI("Publishing data into Google Sheets...");  
-
-  // Update counter
-  value0 ++;
-  value1 = temperatureA;
-  value2 = temperatureB;
-  value3 = powerA;
-  value4 = powerB;
-  value5 = targetTempA;
-  value6 = targetTempB;
-  // value7 = sensorError;
-  value8 = ESP.getFreeHeap(); 
-
-  // prepare payload
-  // payload = payload_base + "\"" + value0 + "," + value1+ "," + value2 + "," + value3 + "," + value4 + "," + value5 + "," + value6 + ","  + value7 + ","  + value8 +"\"}";
-  sprintf(payload, payload_base, value0, value1, value2, value3, value4, value5, value6, value7, value8);
-
-  if(client->POST(url, host, payload, false)){ 
-    // Published data to Google Sheets
-    debugV(" - Payload >> %s\n", payload);
-  }
-  else{
-    debugE(" - Error while connecting\n");
-  }
-}
-
 void get_data_from_sensors(){
   
   debugI("Retrieve data from sensor...");
@@ -478,122 +378,65 @@ void set_heater_power_with_PID(){
   debugV(" - PWM   >> Heater A:%d\tHeater B:%d\n", pwmA, pwmB);
 }
 
-void post_data_into_influxdb(){
+void publish_data_to_adafruit_io(){
   // Log activity
-  debugI("Writing into Influx\n");
+  debugI("Publishing to Adafruit.io\n");
 
-  // Clear fields for reusing the point. Tags will remain the same as set above.
-  dpLabA.clearFields();
-  dpLabA.addField("Temperature", temperatureA);
-  dpLabA.addField("Power", powerA*100.0);
-  dpLabA.addField("Target", targetTempA);
+  feed_tempA->save(temperatureA);
+  feed_powerA->save(powerA*100.0);
+  feed_targetA->save(targetTempA);
 
-  dpLabB.clearFields();
-  dpLabB.addField("Temperature", temperatureB);
-  dpLabB.addField("Power", powerB*100.0);
-  dpLabB.addField("Target", targetTempB);
-
-  dpMCU.clearFields();
-  dpMCU.addField("Error", value7);
-  dpMCU.addField("FreeHeap", ESP.getFreeHeap());
-  dpMCU.addField("DebugCounter", debugcounter);  
-
-  // Check WiFi connection and reconnect if needed
-  if (wifiMulti.run() != WL_CONNECTED) {
-    debugE("Wifi connection lost");
-  }
-
-  // Write point
-  if (!influx_client.writePoint(dpLabA)) {
-    debugE("InfluxDB LabA write failed: %s\n", influx_client.getLastErrorMessage());
-  }
-  if (!influx_client.writePoint(dpLabB)) {
-    debugE("InfluxDB LabB write failed: %s\n", influx_client.getLastErrorMessage());
-  }
-  if (!influx_client.writePoint(dpMCU)) {
-    debugE("InfluxDB MCU write failed: %s\n", influx_client.getLastErrorMessage());
-  }
+  feed_tempB->save(temperatureB);
+  feed_powerB->save(powerB*100.0);
+  feed_targetB->save(targetTempB);
 }
 
-void get_data_from_influxdb(){
-  debugI("Getting data from Influxdb...");
-  targetTempA = query_influx_sensor("target", "BoxA");
-  targetTempB = query_influx_sensor("target", "BoxB");
-  time_delay = (int) query_influx_device("delay", "NodeMCU-ESP12E");
-  max_duty_cycleA = (int) query_influx_sensor("maxduty", "BoxA");
-  max_duty_cycleB = (int) query_influx_sensor("maxduty", "BoxB");
+void handleMessage_delay(AdafruitIO_Data *data) {
+  debugI("Receiving published feed... - Delay");
+  time_delay = (int) data->toFloat();
+  debugV(" - New delay value: %u", time_delay);
+}
 
-  new_Kp = query_influx_sensor("Kp", "BoxA");
-  new_Ki = query_influx_sensor("Ki", "BoxA");
-  new_Kd = query_influx_sensor("Kd", "BoxA");
-  if ( Kp_A != new_Kp ||  Kd_A != new_Kd || Ki_A != new_Ki){
+void handleMessage_configA(AdafruitIO_Data *data) {
+  debugI("Receiving published feed... - Config A");
+  json_response = data->toString();
+  deserializeJson(doc, json_response);    
+  debugV(" - json >> %u", json_response.length());  
+  debugV(" - Kd >> %f", doc["Kd"]);
+
+  targetTempA = doc["t"];
+  max_duty_cycleA = (int) doc["m"];
+
+  if ( Kp_A != doc["p"] ||  Kd_A != doc["d"] || Ki_A != doc["i"]){
     flagChangeKA = true;
-    Kp_A = new_Kp;
-    Kd_A = new_Kd;
-    Ki_A = new_Ki;
+    Kp_A = doc["p"];
+    Kd_A = doc["d"];
+    Ki_A = doc["i"];
   } else {
     flagChangeKA = false;
-  }
-
-  new_Kp = query_influx_sensor("Kp", "BoxB");
-  new_Ki = query_influx_sensor("Ki", "BoxB");
-  new_Kd = query_influx_sensor("Kd", "BoxB");
-  if ( Kp_B != new_Kp ||  Kd_B != new_Kd || Ki_B != new_Ki){
-    flagChangeKB = true;
-    Kp_B = new_Kp;
-    Kd_B = new_Kd;
-    Ki_B = new_Ki;
-  } else {
-    flagChangeKB = false;
   }  
 
-  debugV(" - Data >> Target A:%f\tTarget B:%f\tDelay:%d\tDuty A:%d\tDuty B:%d", targetTempA, targetTempB, time_delay, max_duty_cycleA, max_duty_cycleB);
-  debugV(" - Data >> A(Kp, Ki, Kd):(%f , %f, %f)\tB(Kp, Ki, Kd):(%f , %f, %f)\n", Kp_A, Ki_A, Kd_A, Kp_B, Ki_B, Kd_B);  
+  debugV(" - Data A >> Target: %f\tDuty: %d\t(Kp, Ki, Kd):(%f , %f, %f)\n", targetTempA, max_duty_cycleA, Kp_A, Ki_A, Kd_A);
 }
 
-double query_influx_sensor(String field, String sensor){
-  // Construct a Flux query
-  influx_query = "from(bucket: \"LabOven\") |> range(start: -10y) |> filter(fn: (r) => r._measurement == \"SensorConfig\" and r.sensor == \"" + sensor + "\" and r._field == \"" + field + "\") |> last() |> yield()" ;
+void handleMessage_configB(AdafruitIO_Data *data) {
+  debugI("Receiving published feed... - Config B");
+  json_response = data->toString();
+  deserializeJson(doc, json_response);    
 
-  // Send query to the server and get result
-  FluxQueryResult influx_result = influx_client.query(influx_query);
+  targetTempB = doc["t"];
+  max_duty_cycleB = (int) doc["m"];
 
-  influx_result.next();
-  double result =  influx_result.getValueByName("_value").getDouble(); 
+  if ( Kp_B != doc["p"] ||  Kd_B != doc["d"] || Ki_B != doc["i"]){
+    flagChangeKB = true;
+    Kp_B = doc["p"];
+    Kd_B = doc["d"];
+    Ki_B = doc["i"];
+  } else {
+    flagChangeKB = false;
+  }
 
-  return result;
-}
-
-double query_influx_device(String field, String device){
-  // Construct a Flux query
-  influx_query = "from(bucket: \"LabOven\") |> range(start: -10y) |> filter(fn: (r) => r._measurement == \"SensorConfig\" and r.device == \"" + device + "\" and r._field == \"" + field + "\") |> last() |> yield()" ;
-
-  // Send query to the server and get result
-  FluxQueryResult influx_result = influx_client.query(influx_query);
-
-  influx_result.next();
-  double result =  influx_result.getValueByName("_value").getDouble(); 
-
-  return result;
-}
-
-
-void query_influx(){
-  debugI("Getting data from Influxdb...");
-
-  // Construct a Flux query
-  // influx_query = "from(bucket: \"LabOven\") |> range(start: -10y) |> filter(fn: (r) => r[\"_measurement\"] == \"SensorConfig\") |> last() |> yield()";
-
-  // // Send query to the server and get result
-  // FluxQueryResult influx_result = influx_client.query(influx_query);
-
-  // while (influx_result.next()) {
-  //   field = influx_result.getValueByName("_field").getString();
-  //   sensor = influx_result.getValueByName("sensor").getString();
-  //   device = influx_result.getValueByName("device").getString();
-  //   value = influx_result.getValueByName("_value").getDouble(); 
-  //   debugI("Field: %s = %f\t Sensor: %s\t Device: %s", field, value, sensor, device);
-  // }
+  debugV(" - Data B >> Target: %f\tDuty: %d\t(Kp, Ki, Kd):(%f , %f, %f)\n", targetTempB, max_duty_cycleB, Kp_B, Ki_B, Kd_B);
 }
 
 // -------------------------
